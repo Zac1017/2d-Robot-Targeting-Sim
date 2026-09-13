70692555e41acba52c29c8c95165dac2a6621f05
Zac1017 <9964262@bedfordnhk12.net>
2026-09-13 17:45:53 -0400

    Add robot targeting functionality with distance and angle calculations

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter robot X position: ");
+        double roboX = scanner.nextDouble();
+        System.out.print("Enter robot Y position: ");
+        double roboY = scanner.nextDouble();
+        System.out.print("Enter target X position: ");
+        double targetX = scanner.nextDouble();
+        System.out.print("Enter target Y position: ");
+        double targetY = scanner.nextDouble();
+        System.out.print("Enter robot heading (degrees): ");
+        double heading = scanner.nextDouble();
+
+        Target target = new Target(roboX, roboY, targetX, targetY, heading);
+        
+        System.out.println("Distance to target: " + target.getDistance());
+        System.out.println("Angle to target: " + target.getPlusMinus() + target.getAngleToTarget());
+    }
+}
--- src/Target.java
+++ src/Target.java
@@ -1,3 +1,71 @@
 public class Target {
     
+    public double roboX;
+    public double roboY;
+
+    public double targetX;
+    public double targetY;
+
+    public double heading;
+
+    public Target(double roboX, double roboY, double targetX, double targetY, double heading) {
+        this.roboX = roboX;
+        this.roboY = roboY;
+        this.targetX = targetX;
+        this.targetY = targetY;
+        this.heading = heading;
+    }
+
+    public double getXDistance() {
+        return targetX - roboX;
+    }
+
+    public double getYDistance() {
+        return targetY - roboY;
+    }
+
+    public double getDistance() {
+        return Math.sqrt(Math.pow(getXDistance(), 2) + Math.pow(getYDistance(), 2));   
+    }
+
+    public double getAngleToTarget() {
+        double targetAngle =  Math.toDegrees(Math.atan2(getYDistance(), getXDistance()));
+        double angle = targetAngle - heading;
+
+        while (true) {
+            if (angle < 0) {
+                angle += 360;
+            } else if (angle >= 360) {
+                angle -= 360;
+            } else {
+                break;
+            }
+        }
+        
+
+        return angle;
+    }
+
+    public char getPlusMinus() {
+        double targetAngle = Math.toDegrees(Math.atan2(getYDistance(), getXDistance()));
+        
+        double angle = targetAngle - heading;
+
+        while (true) {
+            if (angle < -180) {
+                angle += 180;
+            } else if (angle >= 180) {
+                angle -= 180;
+            } else {
+                break;
+            }
+        }
+
+        if (angle < 0) {
+            return '-';
+        } else {
+            return '+';
+        }
+
+    }
 }
